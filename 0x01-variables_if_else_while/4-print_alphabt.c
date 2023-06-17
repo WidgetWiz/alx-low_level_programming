c4ea9dcdaa84cf048b7d7d98b92ca12025d6bb5e
ousama <ousama@mail>
2023-06-17 06:33:05 -0700

    can i?.0

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-        char chars[26] = "abcdfghijklmnoprstuvwxyz";
-        int i;
+	char chars[26] = "abcdfghijklmnoprstuvwxyz";
+	int i;
 
-        for (i = 0; i < 24; i++)
-        {
-                putchar(chars[i]);
-        }
-        putchar('\n');
-        return (0);
+	for (i = 0; i < 24; i++)
+	{
+		putchar(chars[i]);
+	}
+	putchar('\n');
+	return (0);
 }
