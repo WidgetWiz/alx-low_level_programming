022adc507b166080988ca845dcca68deb810308d
ousama <ousama@mail>
2023-06-17 10:10:12 -0700

    staying up-to-date

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-
 /**
- * main - Prints numbers from 0 to 9.
+ * maint - Prints alpa in reverse
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
-
-	for (i = 48; i < 58; i++)
+	char ch;
+	for(ch = 'z'; ch >= 'a'; ch--)
 	{
-		putchar(i);
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
